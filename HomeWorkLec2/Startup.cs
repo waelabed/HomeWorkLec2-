8611eea5c512de47d5d49255216040cc4a14d992
wael abed <wael.0@hotmail.com>
2018-03-24 16:42:09 +0200

    first commit

---
+++ HomeWorkLec2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeWorkLec2.Startup))]
+namespace HomeWorkLec2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
